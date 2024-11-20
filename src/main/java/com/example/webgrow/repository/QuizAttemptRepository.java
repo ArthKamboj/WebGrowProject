ba0d4bc642bb27331eebd8c0455657cbf9eb9d13
Arth Kamboj <arth23153015@akgec.ac.in>
2024-11-20 22:32:12 +0530

    Participant quiz api's created

--- src/main/java/com/example/webgrow/repository/QuizAttemptRepository.java
+++ src/main/java/com/example/webgrow/repository/QuizAttemptRepository.java
@@ -1,16 +1,16 @@
-//package com.example.webgrow.repository;
-//
-//import com.example.webgrow.models.QuizAttempt;
-//import com.example.webgrow.models.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Repository
-//public interface QuizAttemptRepository extends JpaRepository<QuizAttempt, Long> {
-//
-//    Optional<QuizAttempt> findByParticipantAndQuiz(User participant, Long quizId);
-//
-//}
+package com.example.webgrow.repository;
+
+import com.example.webgrow.models.Quiz;
+import com.example.webgrow.models.QuizAttempt;
+import com.example.webgrow.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface QuizAttemptRepository extends JpaRepository<QuizAttempt, Long> {
+
+    Optional<QuizAttempt> findByParticipantAndQuiz(User participant, Quiz quiz);
+
+}
