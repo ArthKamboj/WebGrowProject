820c1b01b82b91a1b4cd30fef868cd7d5edec2cb
anubhavhajela4 <anubhavhajela4@gmail.com>
2024-11-24 12:34:16 +0530

    Added controller class to upload image

--- src/main/java/com/example/webgrow/configuration/S3Configuration.java
+++ src/main/java/com/example/webgrow/configuration/S3Configuration.java
@@ -24,6 +24,5 @@ public class S3Configuration {
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
-
     }
 }
