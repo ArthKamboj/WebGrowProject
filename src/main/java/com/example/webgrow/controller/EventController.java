aa0fcfbbca514d04b58fad3ac88285b6f8fc7e09
anubhavhajela4 <anubhavhajela4@gmail.com>
2024-11-28 18:25:29 +0530

    Corrected Pagination logic

--- src/main/java/com/example/webgrow/controller/EventController.java
+++ src/main/java/com/example/webgrow/controller/EventController.java
@@ -7,6 +7,7 @@ import com.example.webgrow.payload.request.EventRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -47,7 +48,7 @@ public class EventController {
     }
 
     @GetMapping("/list")
-    public ResponseEntity<Page<DTOClass>> getAllEvents(Pageable pageable) {
+    public ResponseEntity<Page<DTOClass>> getAllEvents( Pageable pageable) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String email = authentication.getName();
         Page<DTOClass> response = eventService.getEventList(email, pageable);
